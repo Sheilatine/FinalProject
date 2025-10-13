4da74968d0178ae8e1fcafc88068ecf9f8aa4985
Sheila Christine <146451275+Sheilatine@users.noreply.github.com>
2025-10-13 14:37:05 +0300

    Update seed.js

--- seed.js
+++ seed.js
@@ -10,8 +10,8 @@ async function main() {
     await User.deleteMany({});
 
     await User.insertMany([
-        { name: "Rose Wambui", email: "alice@example.com", role: "admin" },
-        { name: "Dedan Okware", email: "dedan@plp.com" },
+        { name: "Rose Wambui", email: "alice@example.com", password: Rose@25, phone_number: 0797563426, university: UON, role: "admin" },
+        { name: "Dedan Okware", email: "dedan@plp.com", password: Dedan@25, phone_number: 0742367812, university: JKUAT },
     ]);
 
    
