b5de9290cdf1bf4007c43f7aa72425eb8f54e2c5
Sheila Christine <146451275+Sheilatine@users.noreply.github.com>
2025-10-13 17:36:14 +0300

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,8 @@
 // draft a schema
 const {mongoose} = require('../db'); //importing mongoose from db.js file
 const {Schema} = mongoose; // importing schema from mongoose
+const bcrypt = require('bcryptjs');
+
 
 
 
@@ -13,6 +15,13 @@ const userSchema = new Schema({
     university: {type: String, required: true},
     role: {type: String, default: "user"}
 }, {timestamps: true});
+
+// Pre-save hook: hashes password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // only hash if password is new/changed
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 
 //create a model(represents the collections aka tables)
